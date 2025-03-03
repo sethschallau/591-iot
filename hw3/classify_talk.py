2f2588440bc9923e940b0abc728f4fa48a0538f7
Seth Schallau <seth.schallau@vitalsource.com>
2025-03-02 21:45:22 -0500

    pull from the topic, publish to the next one

--- hw3/classify_talk.py
+++ hw3/classify_talk.py
@@ -1,3 +1,31 @@
+import paho.mqtt.client as mqtt
+
 BROKER = "broker.hivemq.com"
 PORT = 1883
-TOPIC = "sethschallaudoor"
+SUBSCRIBE_TOPIC = "sethschallaudoor"
+PUBLISH_TOPIC = "sethschallauinterface"
+
+# def load_classifier():
+#     #classifier from file (example: "classifier.pkl")
+#     return None
+
+def classify_message(payload):
+
+    return payload
+
+def on_message(client, userdata, message):
+    received_payload = message.payload.decode()
+    print(f"Received: {received_payload}")
+
+    classification_result = classify_message(received_payload)
+
+    client.publish(PUBLISH_TOPIC, classification_result)
+
+client = mqtt.Client()
+client.on_message = on_message
+
+client.connect(BROKER, PORT)
+client.subscribe(SUBSCRIBE_TOPIC)
+print(f"Subscribed to {SUBSCRIBE_TOPIC}")
+
+client.loop_forever()
