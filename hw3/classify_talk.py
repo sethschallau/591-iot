cc28eddd890019cbb02ab428d8fc7e3fa9ef1b58
Seth Schallau <sethmschallau@gmail.com>
2025-03-14 21:14:21 -0400

    loading the classifier file, interpreting the results, and using a state machine that hopefully doesnt backfire on us

--- hw3/classify_talk.py
+++ hw3/classify_talk.py
@@ -1,24 +1,52 @@
 import paho.mqtt.client as mqtt
+import pickle
+import json
+import numpy as np
 
 BROKER = "broker.hivemq.com"
 PORT = 1883
 SUBSCRIBE_TOPIC = "sethschallaudoor"
 PUBLISH_TOPIC = "sethschallauinterface"
 
-# def load_classifier():
-#     #classifier from file (example: "classifier.pkl")
-#     return None
+class ClosedDoor:
+    def state(self):
+        return "closed"
+
+    def next_state(self):
+        return OpenDoor()
+
+class OpenDoor:
+    def state(self):
+        return "open"
+
+    def next_state(self):
+        return ClosedDoor()
+
+current_door = ClosedDoor()
+
+def load_classifier():
+    with open("svm_model.pkl", "rb") as f:
+        return pickle.load(f)
+
+classifier = load_classifier()
 
 def classify_message(payload):
-
-    return payload
+    data = json.loads(payload)
+    features = np.array([[data["ax"], data["ay"], data["az"], data["gx"], data["gy"], data["gz"]]])
+    prediction = classifier.predict(features)[0]
+    return "stable" if prediction == 0 else "moving"
 
 def on_message(client, userdata, message):
+    global current_door
     received_payload = message.payload.decode()
 
     classification_result = classify_message(received_payload)
+    
+    if classification_result == "moving":
+        current_door = current_door.next_state()
+        client.publish(PUBLISH_TOPIC, current_door.state())
 
-    client.publish(PUBLISH_TOPIC, classification_result)
+    
 
 client = mqtt.Client()
 client.on_message = on_message
